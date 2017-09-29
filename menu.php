641c9f006f81a0e86c71b7e719d0ca43b2a89a4f
heyharuna <30873223+heyharuna@users.noreply.github.com>
2017-09-28 18:23:58 -0700

    menu file

---
+++ menu.php
@@ -0,0 +1,19 @@
+
+<div id="overlayMenu" >
+  <div class="nav">
+    <img src="img/logo/logo.png" class="topLogo">
+    <img src="img/home/close.png" class="menubar close" id="menu">
+  </div>
+  <div id="menuPage">
+    <ul>
+      <li><a href="index.php" style="color:#00FFC5;">home</a></li>
+      <li><a href="news.php" style="color:#BA0007;">news</a></li>
+      <li><a href="tourschedule.php" style="color:#FF5800;">tour schedule</a></li>
+      <li><a href="record.php" style="color:#EA48EA;">record</a></li>
+      <li><a href="video.php" style="color:#2BBA00;">video</a></li>
+      <li><a href="gallery.php" style="color:#5D00BA;">gallery</a></li>
+      <li><a href="contact.php" style="color:#EAE448;">contact</a></li>
+    </ul>
+    <!-- <button id="closeBtn">Close</button> -->
+  </div>
+</div>
